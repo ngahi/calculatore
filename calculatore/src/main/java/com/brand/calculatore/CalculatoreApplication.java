d225eee82d8f8d6086a0c9c840e9ed338a3da9ce
idriss <ngahibrandon@gmail.com>
2025-07-17 10:59:00 +0100

    Ajouter le squelette Spring Boot

---
+++ calculatore/src/main/java/com/brand/calculatore/CalculatoreApplication.java
@@ -0,0 +1,13 @@
+package com.brand.calculatore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brand/calculatore/CalculatoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brand.calculatore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
